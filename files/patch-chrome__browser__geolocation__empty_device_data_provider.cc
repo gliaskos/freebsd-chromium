375e462d7936b9deab365e25c11403a3ad97cf69
Rene Ladan <rene@FreeBSD.org>
2011-01-07 21:39:26 +0000

    fix patch for empty_device_data_provider.cc

--- files/patch-chrome__browser__geolocation__empty_device_data_provider.cc
+++ files/patch-chrome__browser__geolocation__empty_device_data_provider.cc
@@ -1,11 +1,11 @@
---- ./chrome/browser/geolocation/empty_device_data_provider.cc.orig     2010-12-13 12:04:26.000000000 +0100 
-+++ ./chrome/browser/geolocation/empty_device_data_provider.cc  2010-12-20 20:41:37.000000000 +0100 
-@@ -12,7 +12,7 @@ 
- } 
-
- // Only define for platforms that lack a real wifi data provider. 
--#if !defined(OS_WIN) && !defined(OS_MACOSX) && !defined(OS_LINUX) 
-+#if !defined(OS_WIN) && !defined(OS_MACOSX) && !defined(OS_LINUX) || defined(OS_FREEBSD) 
- // static 
- template<> 
- WifiDataProviderImplBase* WifiDataProvider::DefaultFactoryFunction() { 
+--- chrome/browser/geolocation/empty_device_data_provider.cc.orig	2010-12-13 12:04:26.000000000 +0100
++++ chrome/browser/geolocation/empty_device_data_provider.cc	2011-01-07 22:36:05.000000000 +0100
+@@ -12,7 +12,7 @@
+ }
+ 
+ // Only define for platforms that lack a real wifi data provider.
+-#if !defined(OS_WIN) && !defined(OS_MACOSX) && !defined(OS_LINUX)
++#if !defined(OS_WIN) && !defined(OS_MACOSX) && !defined(OS_LINUX) && !defined(OS_FREEBSD)
+ // static
+ template<>
+ WifiDataProviderImplBase* WifiDataProvider::DefaultFactoryFunction() {
