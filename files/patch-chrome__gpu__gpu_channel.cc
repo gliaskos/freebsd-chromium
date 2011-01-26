3c6c444418ab698e8b5b8a3edcd7a7afaec1d75c
Rene Ladan <rene@FreeBSD.org>
2011-01-26 13:40:02 +0000

    update to 9.0.597.83

--- files/patch-chrome__gpu__gpu_channel.cc
+++ files/patch-chrome__gpu__gpu_channel.cc
@@ -1,9 +1,9 @@
---- ./chrome/gpu/gpu_channel.cc.orig	2010-12-13 12:04:29.000000000 +0100
-+++ ./chrome/gpu/gpu_channel.cc	2010-12-20 20:41:37.000000000 +0100
-@@ -138,7 +138,7 @@
-   // the GpuCommandBufferStub, we will create a child window within the view
-   // HWND into which we will render.
-   handle = view;
+--- chrome/gpu/gpu_channel.cc.orig	2011-01-25 12:41:15.000000000 +0100
++++ chrome/gpu/gpu_channel.cc	2011-01-26 14:38:40.000000000 +0100
+@@ -148,7 +148,7 @@
+   // offscreen rendering and the browser provides whichever platform specific
+   // "render target" the GpuCommandBufferStub targets.
+   handle = gfx::NativeViewFromId(view_id);
 -#elif defined(OS_LINUX)
 +#elif defined(OS_LINUX) || defined(OS_FREEBSD)
    ChildThread* gpu_thread = ChildThread::current();
