211c5ac9ca20882bab717542840a5a9a6c48f9c9
George Liaskos <geo.liaskos@gmail.com>
2012-05-25 23:32:28 +0000

    Fix a bug which caused chromium to crash on startup. Use the system allocator (jemalloc) instead of tcmalloc which miscompiles with clang.

--- files/patch-chrome__common__chrome_paths.cc
+++ files/patch-chrome__common__chrome_paths.cc
@@ -1,11 +1,16 @@
---- chrome/common/chrome_paths.cc.orig	2012-05-19 23:49:01.042717907 +0300
-+++ chrome/common/chrome_paths.cc	2012-05-19 23:50:34.411106139 +0300
-@@ -334,7 +334,7 @@
+--- chrome/common/chrome_paths.cc.orig	2012-05-23 04:50:48.000000000 +0300
++++ chrome/common/chrome_paths.cc	2012-05-26 00:47:40.880289194 +0300
+@@ -334,10 +334,12 @@
        if (!file_util::PathExists(cur))  // We don't want to create this
          return false;
        break;
 -#if defined(OS_POSIX) && !defined(OS_MACOSX) && !defined(OS_OPENBSD)
-+#if defined(OS_POSIX) && !defined(OS_MACOSX) && !defined(OS_BSD)
++#if defined(OS_POSIX) && !defined(OS_MACOSX)
      case chrome::DIR_POLICY_FILES: {
  #if defined(GOOGLE_CHROME_BUILD)
        cur = FilePath(FILE_PATH_LITERAL("/etc/opt/chrome/policies"));
++#elif defined(OS_BSD)
++      cur = FilePath(FILE_PATH_LITERAL("/usr/local/etc/chrome/policies"));
+ #else
+       cur = FilePath(FILE_PATH_LITERAL("/etc/chromium/policies"));
+ #endif
