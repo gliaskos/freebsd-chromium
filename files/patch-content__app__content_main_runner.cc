94e9b96548fb7e50c579d1d80068d12ad85f9e90
George Liaskos <geo.liaskos@gmail.com>
2012-12-06 00:42:08 +0200

    Patch cleanup, don't use ZygoteProcess switch

--- files/patch-content__app__content_main_runner.cc
+++ files/patch-content__app__content_main_runner.cc
@@ -1,5 +1,5 @@
---- content/app/content_main_runner.cc.orig	2012-10-31 21:01:34.000000000 +0200
-+++ content/app/content_main_runner.cc	2012-11-07 17:25:10.000000000 +0200
+--- content/app/content_main_runner.cc.orig	2012-11-28 04:01:23.000000000 +0200
++++ content/app/content_main_runner.cc	2012-12-04 23:40:45.000000000 +0200
 @@ -91,7 +91,7 @@
  extern int RendererMain(const content::MainFunctionParams&);
  extern int WorkerMain(const content::MainFunctionParams&);
@@ -27,12 +27,3 @@
    // Zygote startup is special -- see RunZygote comments above
    // for why we don't use ZygoteMain directly.
    if (process_type == switches::kZygoteProcess)
-@@ -548,7 +548,7 @@
-                kPrimaryIPCChannel + base::GlobalDescriptors::kBaseDescriptor);
- #endif  // !OS_ANDROID && !OS_IOS
- 
--#if defined(OS_LINUX) || defined(OS_OPENBSD)
-+#if defined(OS_LINUX) || defined(OS_BSD)
-     g_fds->Set(kCrashDumpSignal,
-                kCrashDumpSignal + base::GlobalDescriptors::kBaseDescriptor);
- #endif
