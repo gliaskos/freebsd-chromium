8a50c5572eb47c6413922e3214056c4ade415587
René Ladan <rene@freebsd.org>
2013-10-10 14:28:01 +0200

    Revert "Try to fix build on recent CURRENT, from log at"
    This reverts commit e00e69d3a17e648c489d5cae166f7f6bcb8bff1a.
    
    Fixes for 10.0 (mostly libc++) will first go into the freebsd-current
    branch.

--- www/chromium/files/patch-base__process__process_metrics_freebsd.cc
+++ www/chromium/files/patch-base__process__process_metrics_freebsd.cc
@@ -1,12 +1,10 @@
---- ./base/process/process_metrics_freebsd.cc.orig	2013-09-03 09:09:03.000000000 +0200
-+++ ./base/process/process_metrics_freebsd.cc	2013-09-07 21:30:29.000000000 +0200
-@@ -4,6 +4,10 @@
+--- base/process/process_metrics_freebsd.cc.orig	2013-08-17 19:21:48.000000000 +0300
++++ base/process/process_metrics_freebsd.cc	2013-08-17 19:22:18.000000000 +0300
+@@ -4,6 +4,8 @@
  
  #include "base/process/process_metrics.h"
  
 +#include "base/sys_info.h"
-+
-+#include <unistd.h> /* getpagesize() */
 +
  namespace base {
  
