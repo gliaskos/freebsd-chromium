ed0a0f67e44061abade122f5649909770564e5b6
René Ladan <r.c.ladan@gmail.com>
2014-09-11 15:09:56 +0200

    This should work, but it doesn't without the big hammer.

--- www/chromium/files/patch-third_party__webrtc__base__stringutils.h
+++ www/chromium/files/patch-third_party__webrtc__base__stringutils.h
@@ -1,11 +1,11 @@
---- third_party/webrtc/base/stringutils.h.orig	2014-09-04 00:29:43 UTC
-+++ third_party/webrtc/base/stringutils.h
+--- third_party/webrtc/base/stringutils.h.orig	2014-09-11 08:59:25.000000000 +0200
++++ third_party/webrtc/base/stringutils.h	2014-09-11 13:32:35.000000000 +0200
 @@ -23,11 +23,11 @@
  #endif  // WEBRTC_WIN 
  
  #if defined(WEBRTC_POSIX)
 -#ifdef BSD
-+#ifdef WEBRTC_BSD
++#if defined(WEBRTC_BSD) || defined(BSD)
  #include <stdlib.h>
 -#else  // BSD
 +#else  // WEBRTC_BSD
