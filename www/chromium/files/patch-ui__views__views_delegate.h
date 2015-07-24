9c37f7a0ec492d6083c2211ca9fd87934853f7db
Charlie Root <tmvfroid@gmail.com>
2015-07-24 18:27:18 -0400

    Updated the makefile and pkg-plist files to properly stage and install chromium 44.0.2403.89 Forgot to commit a ui related patch.

--- www/chromium/files/patch-ui__views__views_delegate.h
+++ www/chromium/files/patch-ui__views__views_delegate.h
@@ -1,5 +1,14 @@
---- ui/views/views_delegate.h.orig	2015-07-22 22:02:35.028909000 -0400
-+++ ui/views/views_delegate.h	2015-07-22 22:03:21.321036000 -0400
+--- ui/views/views_delegate.h.orig	2015-07-15 16:30:11.000000000 -0400
++++ ui/views/views_delegate.h	2015-07-24 06:37:19.269166000 -0400
+@@ -101,7 +101,7 @@
+   // Returns true if the window passed in is in the Windows 8 metro
+   // environment.
+   virtual bool IsWindowInMetro(gfx::NativeWindow window) const;
+-#elif defined(OS_LINUX) && !defined(OS_CHROMEOS)
++#elif defined(OS_LINUX) && !defined(OS_CHROMEOS) || defined(OS_FREEBSD)
+   virtual gfx::ImageSkia* GetDefaultWindowIcon() const;
+ #endif
+ 
 @@ -139,7 +139,7 @@
    // Returns the user-visible name of the application.
    virtual std::string GetApplicationName();
