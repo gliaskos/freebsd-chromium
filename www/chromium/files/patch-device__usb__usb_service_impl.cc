20f6ec068ca895ffc863d2a7d453ed091123af6d
Charlie Root <tmvfroid@gmail.com>
2015-07-23 21:05:19 -0400

    Chromium version 44.0.2403.89 now builds and runs successfully on FreeBSD. Still needs testing for potential errors.

--- www/chromium/files/patch-device__usb__usb_service_impl.cc
+++ www/chromium/files/patch-device__usb__usb_service_impl.cc
@@ -1,6 +1,18 @@
 --- device/usb/usb_service_impl.cc.orig	2015-07-15 16:30:04.000000000 -0400
-+++ device/usb/usb_service_impl.cc	2015-07-22 07:39:42.394751000 -0400
-@@ -294,6 +294,7 @@
++++ device/usb/usb_service_impl.cc	2015-07-22 20:14:21.946835000 -0400
+@@ -17,7 +17,11 @@
+ #include "base/thread_task_runner_handle.h"
+ #include "components/device_event_log/device_event_log.h"
+ #include "device/usb/usb_error.h"
++#if defined(OS_FREEBSD)
++#include "libusb.h"
++#else
+ #include "third_party/libusb/src/libusb/libusb.h"
++#endif
+ 
+ #if defined(OS_WIN)
+ #include <setupapi.h>
+@@ -294,6 +298,7 @@
        weak_factory_(this) {
    base::MessageLoop::current()->AddDestructionObserver(this);
  
@@ -8,7 +20,7 @@
    int rv = libusb_hotplug_register_callback(
        context_->context(),
        static_cast<libusb_hotplug_event>(LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED |
-@@ -321,14 +322,16 @@
+@@ -321,14 +326,16 @@
      }
  #endif  // OS_WIN
    }
@@ -26,7 +38,7 @@
    for (const auto& map_entry : devices_) {
      map_entry.second->OnDisconnect();
    }
-@@ -566,6 +569,7 @@
+@@ -566,6 +573,7 @@
    device->OnDisconnect();
  }
  
@@ -34,7 +46,7 @@
  // static
  int LIBUSB_CALL UsbServiceImpl::HotplugCallback(libusb_context* context,
                                                  PlatformUsbDevice device,
-@@ -603,6 +607,7 @@
+@@ -603,6 +611,7 @@
  
    return 0;
  }
--- www/chromium/files/patch-ui__views__views_delegate.h
+++ www/chromium/files/patch-ui__views__views_delegate.h
@@ -1,11 +1,11 @@
---- ui/views/views_delegate.h.orig	2014-10-10 08:54:19 UTC
-+++ ui/views/views_delegate.h
-@@ -98,7 +98,7 @@
-   // Returns true if the window passed in is in the Windows 8 metro
-   // environment.
-   virtual bool IsWindowInMetro(gfx::NativeWindow window) const;
--#elif defined(OS_LINUX) && !defined(OS_CHROMEOS)
-+#elif (defined(OS_LINUX) && !defined(OS_CHROMEOS)) || defined(OS_FREEBSD)
-   virtual gfx::ImageSkia* GetDefaultWindowIcon() const;
- #endif
+--- ui/views/views_delegate.h.orig	2015-07-22 22:02:35.028909000 -0400
++++ ui/views/views_delegate.h	2015-07-22 22:03:21.321036000 -0400
+@@ -139,7 +139,7 @@
+   // Returns the user-visible name of the application.
+   virtual std::string GetApplicationName();
  
+-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
++#if defined(OS_LINUX) && !defined(OS_CHROMEOS) || defined(OS_BSD)
+   // Get a task runner suitable for posting initialization tasks for
+   // Aura Linux accessibility.
+   virtual scoped_refptr<base::TaskRunner>
