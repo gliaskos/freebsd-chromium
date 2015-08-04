8c0798140a4629f0b8bef00bbcad6df7d0370bc6
René Ladan <r.c.ladan@gmail.com>
2015-08-04 19:49:29 +0200

    Prune more FreeBSD 8

--- www/chromium/files/patch-device__usb__usb_device_handle_impl.cc
+++ www/chromium/files/patch-device__usb__usb_device_handle_impl.cc
@@ -1,11 +1,10 @@
 --- device/usb/usb_device_handle_impl.cc.orig	2014-10-10 08:54:15 UTC
 +++ device/usb/usb_device_handle_impl.cc
-@@ -19,7 +19,13 @@
+@@ -19,7 +19,12 @@
  #include "device/usb/usb_device_impl.h"
  #include "device/usb/usb_error.h"
  #include "device/usb/usb_service.h"
 +#if defined(OS_FREEBSD)
-+#include <osreldate.h>
 +#include "libusb.h"
 +#define LIBUSB_CALL
 +#else
