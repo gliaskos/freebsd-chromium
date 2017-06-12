bce375082bc786e1b73f1d4e8dd757e152daf063
Carlos J <cjpm@gmx.us>
2017-06-12 17:59:51 +0200

    Get rid of multiple definition errors and check compiler options

--- www/chromium/files/patch-base_process_memory__stubs.cc
+++ www/chromium/files/patch-base_process_memory__stubs.cc
@@ -1,6 +1,23 @@
---- base/process/memory_stubs.cc.orig	2017-06-05 19:03:00 UTC
-+++ base/process/memory_stubs.cc
-@@ -31,6 +31,8 @@ void TerminateBecauseOutOfMemory(size_t size) {
+--- base/process/memory_stubs.cc.orig	2017-06-05 21:03:00.000000000 +0200
++++ base/process/memory_stubs.cc	2017-06-12 17:54:06.034577000 +0200
+@@ -9,6 +9,8 @@
+ 
+ namespace base {
+ 
++#if !defined(OS_FREEBSD)
++// Get rid of multiple definition errors
+ void EnableTerminationOnOutOfMemory() {
+ }
+ 
+@@ -22,6 +24,7 @@
+ void TerminateBecauseOutOfMemory(size_t size) {
+   abort();
+ }
++#endif
+ 
+ // UncheckedMalloc and Calloc exist so that platforms making use of
+ // EnableTerminationOnOutOfMemory have a way to allocate memory without
+@@ -31,6 +34,8 @@
  // their respective stdlib function since those functions will return null on a
  // failure to allocate.
  
@@ -9,7 +26,7 @@
  bool UncheckedMalloc(size_t size, void** result) {
    *result = malloc(size);
    return *result != nullptr;
-@@ -40,5 +42,6 @@ bool UncheckedCalloc(size_t num_items, size_t size, vo
+@@ -40,5 +45,6 @@
    *result = calloc(num_items, size);
    return *result != nullptr;
  }
