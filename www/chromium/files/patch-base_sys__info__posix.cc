67ab93ecd882601edb2a2fd23218ff77ec367cb7
Carlos J <cjpm@gmx.us>
2017-07-01 02:04:59 +0200

    Add arm64 support

--- www/chromium/files/patch-base_sys__info__posix.cc
+++ www/chromium/files/patch-base_sys__info__posix.cc
@@ -1,5 +1,5 @@
---- base/sys_info_posix.cc.orig	2017-06-05 19:03:00 UTC
-+++ base/sys_info_posix.cc
+--- base/sys_info_posix.cc.orig	2017-06-26 21:03:13.000000000 +0200
++++ base/sys_info_posix.cc	2017-07-01 01:16:10.672118000 +0200
 @@ -35,7 +35,7 @@
  
  namespace {
@@ -9,7 +9,7 @@
  int NumberOfProcessors() {
    // sysconf returns the number of "logical" (not "physical") processors on both
    // Mac and Linux.  So we get the number of max available "logical" processors.
-@@ -128,7 +128,7 @@ bool GetDiskSpaceInfo(const base::FilePath& path,
+@@ -128,7 +128,7 @@
  
  namespace base {
  
@@ -17,4 +17,13 @@
 +#if !defined(OS_BSD)
  int SysInfo::NumberOfProcessors() {
    return g_lazy_number_of_processors.Get().value();
+ }
+@@ -219,6 +219,8 @@
+     arch = "x86";
+   } else if (arch == "amd64") {
+     arch = "x86_64";
++  } else if (arch == "arm64") {
++    arch = "aarch64";
+   }
+   return arch;
  }
