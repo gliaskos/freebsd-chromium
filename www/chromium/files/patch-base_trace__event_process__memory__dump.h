d920ee029ea0af162254305d6599393d5e220cb0
Carlos J <cjpm@gmx.us>
2018-08-05 21:51:56 +0200

    Update to 68.0.3440.84

--- www/chromium/files/patch-base_trace__event_process__memory__dump.h
+++ www/chromium/files/patch-base_trace__event_process__memory__dump.h
@@ -1,11 +1,11 @@
---- base/trace_event/process_memory_dump.h.orig	2017-04-19 19:06:28 UTC
-+++ base/trace_event/process_memory_dump.h
-@@ -24,7 +24,7 @@
+--- base/trace_event/process_memory_dump.h.orig	2018-08-01 00:08:26.000000000 +0200
++++ base/trace_event/process_memory_dump.h	2018-08-04 13:49:22.487126000 +0200
+@@ -22,7 +22,7 @@
  
  // Define COUNT_RESIDENT_BYTES_SUPPORTED if platform supports counting of the
  // resident memory.
--#if (defined(OS_POSIX) && !defined(OS_NACL)) || defined(OS_WIN)
-+#if (defined(OS_POSIX) && !defined(OS_NACL) && !defined(OS_BSD)) || defined(OS_WIN)
+-#if !defined(OS_NACL)
++#if !defined(OS_NACL) && !defined(OS_BSD)
  #define COUNT_RESIDENT_BYTES_SUPPORTED
  #endif
  
