891fd52bc0cb0a1d1e335463621da252aca47d37
Carlos J <cjpm@gmx.us>
2018-08-29 20:31:31 +0200

    Add more improvements to TEST option

--- www/chromium/files/patch-base_task_scheduler_scheduler_single_thread_task_runner_manager_unittest.cc
+++ www/chromium/files/patch-base_task_scheduler_scheduler_single_thread_task_runner_manager_unittest.cc
@@ -1,18 +1,12 @@
---- base/task_scheduler/scheduler_single_thread_task_runner_manager_unittest.cc.orig	2018-08-23 19:26:59.907421000 +0200
-+++ base/task_scheduler/scheduler_single_thread_task_runner_manager_unittest.cc	2018-08-23 19:27:50.577418000 +0200
-@@ -301,6 +301,7 @@
+--- base/task_scheduler/scheduler_single_thread_task_runner_manager_unittest.cc.orig	2018-08-08 21:10:31.000000000 +0200
++++ base/task_scheduler/scheduler_single_thread_task_runner_manager_unittest.cc	2018-08-29 19:55:56.573294000 +0200
+@@ -301,6 +301,9 @@
    EXPECT_EQ(ThreadPriority::NORMAL, thread_priority_normal);
  }
  
-+#if !defined(OS_BSD)
++#if defined(OS_BSD)
++#define ThreadNamesSet DISABLED_ThreadNamesSet
++#endif
  TEST_P(TaskSchedulerSingleThreadTaskRunnerManagerCommonTest, ThreadNamesSet) {
    constexpr TaskTraits foo_traits = {TaskPriority::BACKGROUND,
                                       TaskShutdownBehavior::BLOCK_SHUTDOWN};
-@@ -343,6 +344,7 @@
-     EXPECT_NE(std::string::npos, user_blocking_captured_name.find("Shared"));
-   }
- }
-+#endif
- 
- TEST_P(TaskSchedulerSingleThreadTaskRunnerManagerCommonTest,
-        PostTaskAfterShutdown) {
