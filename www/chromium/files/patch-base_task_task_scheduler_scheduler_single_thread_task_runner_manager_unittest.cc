0d84631b965872ae4324f27f649e6fafbd9b154f
Matthias Wolf <matthias@rheinwolf.de>
2018-12-30 14:47:30 +0100

    WIP to update to Chromium 71 - works mostly

--- www/chromium/files/patch-base_task_task_scheduler_scheduler_single_thread_task_runner_manager_unittest.cc
+++ www/chromium/files/patch-base_task_task_scheduler_scheduler_single_thread_task_runner_manager_unittest.cc
@@ -1,11 +1,11 @@
---- base/task/task_scheduler/scheduler_single_thread_task_runner_manager_unittest.cc.orig	2018-12-04 10:49:38.559217000 +0100
-+++ base/task/task_scheduler/scheduler_single_thread_task_runner_manager_unittest.cc	2018-12-04 10:50:09.898539000 +0100
+--- base/task/task_scheduler/scheduler_single_thread_task_runner_manager_unittest.cc.orig	2018-12-12 22:56:01.000000000 +0100
++++ base/task/task_scheduler/scheduler_single_thread_task_runner_manager_unittest.cc	2018-12-16 01:43:12.295373000 +0100
 @@ -295,6 +295,9 @@
    EXPECT_EQ(ThreadPriority::NORMAL, thread_priority_normal);
  }
  
 +#if defined(OS_BSD)
-+#defined ThreadNamesSet DISABLED_ThreadNamesSet
++#define ThreadNamesSet DISABLED_ThreadNamesSet
 +#endif
  TEST_P(TaskSchedulerSingleThreadTaskRunnerManagerCommonTest, ThreadNamesSet) {
    constexpr TaskTraits foo_traits = {TaskPriority::BEST_EFFORT,
