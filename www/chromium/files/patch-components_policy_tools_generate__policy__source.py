d427b4844a6e2e3e23250a443ce4a938089f38b0
Matthias Wolf <matthias@rheinwolf.de>
2019-02-09 19:49:32 +0100

    Update to 72.0.3626.96 and bug fixes

--- www/chromium/files/patch-components_policy_tools_generate__policy__source.py
+++ www/chromium/files/patch-components_policy_tools_generate__policy__source.py
@@ -1,11 +1,11 @@
---- components/policy/tools/generate_policy_source.py.orig	2019-01-30 02:17:55.000000000 +0100
-+++ components/policy/tools/generate_policy_source.py	2019-02-01 00:11:10.092144000 +0100
+--- components/policy/tools/generate_policy_source.py.orig	2019-02-06 23:06:41.000000000 +0100
++++ components/policy/tools/generate_policy_source.py	2019-02-08 20:44:33.267121000 +0100
 @@ -85,7 +85,7 @@
        if platform not in [
            'chrome_frame', 'chrome_os', 'android', 'webview_android',
            'chrome.win', 'chrome.linux', 'chrome.mac', 'chrome.fuchsia',
 -          'chrome.*'
-+          'chrome.bsd', 'chrome.*'
++          'chrome.freebsd', 'chrome.*'
        ]:
          raise RuntimeError('Platform "%s" is not supported' % platform)
  
@@ -14,7 +14,7 @@
          platform_sub = platform[7:]
          if platform_sub == '*':
 -          self.platforms.extend(['win', 'mac', 'linux', 'fuchsia'])
-+          self.platforms.extend(['win', 'mac', 'linux', 'fuchsia', 'bsd'])
++          self.platforms.extend(['win', 'mac', 'linux', 'fuchsia', 'freebsd'])
          else:
            self.platforms.append(platform_sub)
        else:
--- www/chromium/files/patch-content_renderer_renderer__blink__platform__impl.h
+++ www/chromium/files/patch-content_renderer_renderer__blink__platform__impl.h
@@ -1,5 +1,5 @@
---- content/renderer/renderer_blink_platform_impl.h.orig	2019-02-01 07:44:32.951114000 +0100
-+++ content/renderer/renderer_blink_platform_impl.h	2019-02-01 07:45:38.604073000 +0100
+--- content/renderer/renderer_blink_platform_impl.h.orig	2019-01-30 02:18:00.000000000 +0100
++++ content/renderer/renderer_blink_platform_impl.h	2019-02-06 23:14:36.979983000 +0100
 @@ -29,7 +29,7 @@
  #include "third_party/blink/public/platform/modules/cache_storage/cache_storage.mojom.h"
  #include "third_party/blink/public/platform/modules/webdatabase/web_database.mojom.h"
@@ -9,6 +9,15 @@
  #include "components/services/font/public/cpp/font_loader.h"  // nogncheck
  #include "third_party/skia/include/core/SkRefCnt.h"           // nogncheck
  #endif
+@@ -264,7 +264,7 @@
+   std::unique_ptr<service_manager::Connector> connector_;
+   scoped_refptr<base::SingleThreadTaskRunner> io_runner_;
+ 
+-#if defined(OS_LINUX) || defined(OS_MACOSX)
++#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_BSD)
+   std::unique_ptr<blink::WebSandboxSupport> sandbox_support_;
+ #endif
+ 
 @@ -299,7 +299,7 @@
    blink::mojom::CodeCacheHostPtrInfo code_cache_host_info_;
    scoped_refptr<blink::mojom::ThreadSafeCodeCacheHostPtr> code_cache_host_;
