d20a5968f4af3c7c23c896164679476d584356ca
Matthias Wolf <matthias@rheinwolf.de>
2020-04-10 11:08:59 +0200

    Properly merged w/ fixes from version 80

--- www/chromium/files/patch-base_profiler_thread__delegate__posix.cc
+++ www/chromium/files/patch-base_profiler_thread__delegate__posix.cc
@@ -1,20 +1,21 @@
---- base/profiler/thread_delegate_posix.cc.orig	2020-03-03 18:53:47 UTC
+--- base/profiler/thread_delegate_posix.cc.orig	2020-04-10 00:37:03 UTC
 +++ base/profiler/thread_delegate_posix.cc
-@@ -3,6 +3,9 @@
- // found in the LICENSE file.
+@@ -4,6 +4,10 @@
  
  #include <pthread.h>
-+#if defined(__FreeBSD__)
+ 
++#if defined(OS_FREEBSD)
 +#include <pthread_np.h>
 +#endif
- 
++
  #include "base/process/process_handle.h"
  #include "base/profiler/thread_delegate_posix.h"
-@@ -17,12 +20,20 @@ namespace {
+ #include "base/stl_util.h"
+@@ -17,7 +21,12 @@ namespace {
  uintptr_t GetThreadStackBaseAddressImpl(
      SamplingProfilerThreadToken thread_token) {
    pthread_attr_t attr;
-+#if defined(OS_FREEBSD)
++#if defined(OS_FREEBSDD)
 +  pthread_attr_init(&attr);
 +  pthread_attr_get_np(thread_token.pthread_id, &attr);
 +#elif defined(OS_LINUX)
@@ -23,21 +24,7 @@
    // See crbug.com/617730 for limitations of this approach on Linux.
    void* address;
    size_t size;
-   pthread_attr_getstack(&attr, &address, &size);
-   const uintptr_t base_address = reinterpret_cast<uintptr_t>(address) + size;
-+#if defined(OS_FREEBSD)
-+  pthread_attr_destroy(&attr);
-+#endif
-   return base_address;
- }
- 
-@@ -88,11 +99,19 @@ std::vector<uintptr_t*> ThreadDelegatePosix::GetRegist
-   for (size_t i = 19; i <= 29; ++i)
-     registers.push_back(reinterpret_cast<uintptr_t*>(&thread_context->regs[i]));
-   return registers;
--#elif defined(ARCH_CPU_X86_64)  // #if defined(ARCH_CPU_ARM_FAMILY) &&
--                                // defined(ARCH_CPU_32_BITS)
-+#elif defined(ARCH_CPU_X86_64)
+@@ -103,6 +112,15 @@ std::vector<uintptr_t*> ThreadDelegatePosix::GetRegist
    return {
        // Return the set of callee-save registers per the x86-64 System V ABI
        // section 3.2.1, plus the stack pointer.
@@ -53,7 +40,7 @@
        reinterpret_cast<uintptr_t*>(&thread_context->gregs[REG_RBP]),
        reinterpret_cast<uintptr_t*>(&thread_context->gregs[REG_RBX]),
        reinterpret_cast<uintptr_t*>(&thread_context->gregs[REG_R12]),
-@@ -100,6 +119,7 @@ std::vector<uintptr_t*> ThreadDelegatePosix::GetRegist
+@@ -110,6 +128,7 @@ std::vector<uintptr_t*> ThreadDelegatePosix::GetRegist
        reinterpret_cast<uintptr_t*>(&thread_context->gregs[REG_R14]),
        reinterpret_cast<uintptr_t*>(&thread_context->gregs[REG_R15]),
        reinterpret_cast<uintptr_t*>(&thread_context->gregs[REG_RSP]),
